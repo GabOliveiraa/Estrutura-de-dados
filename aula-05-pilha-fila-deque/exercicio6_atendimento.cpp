db44b457fb0f804aa5a81f4cc6a261591df5bd62
GabOliveiraa <122240483+GabOliveiraa@users.noreply.github.com>
2026-08-28 20:14:51 -0300

    Add files via upload

--- aula-05-pilha-fila-deque/exercicio6_atendimento.cpp
+++ aula-05-pilha-fila-deque/exercicio6_atendimento.cpp
@@ -9,7 +9,10 @@ int main() {
     int opcao;
     string nome;
 
+<<<<<<< HEAD
+=======
     // Valores iniciais do seu teste para o sistema já começar preenchido
+>>>>>>> 7a4bfeff7adaf7dfab2644a1560ca9acfa7a0944
     atendimento.push_back("Ana");
     atendimento.push_back("Bruno");
     atendimento.push_front("Carla");
@@ -17,7 +20,10 @@ int main() {
     atendimento.push_front("Elena");
 
     do {
+<<<<<<< HEAD
+=======
         // Exibição do Menu
+>>>>>>> 7a4bfeff7adaf7dfab2644a1560ca9acfa7a0944
         cout << "\n===============================" << endl;
         cout << "    SISTEMA DE ATENDIMENTO     " << endl;
         cout << "===============================" << endl;
@@ -30,28 +36,43 @@ int main() {
         cout << "Escolha uma opcao: ";
         cin >> opcao;
 
+<<<<<<< HEAD
+=======
         // Limpa o buffer do teclado para evitar problemas com o getline
+>>>>>>> 7a4bfeff7adaf7dfab2644a1560ca9acfa7a0944
         cin.ignore(); 
 
         switch (opcao) {
             case 1:
                 cout << "Digite o nome do cliente comum: ";
                 getline(cin, nome);
+<<<<<<< HEAD
+                atendimento.push_back(nome);
+=======
                 atendimento.push_back(nome); // Entra no fim da fila
+>>>>>>> 7a4bfeff7adaf7dfab2644a1560ca9acfa7a0944
                 cout << nome << " adicionado(a) no fim da fila." << endl;
                 break;
 
             case 2:
                 cout << "Digite o nome do cliente prioritario: ";
                 getline(cin, nome);
+<<<<<<< HEAD
+                atendimento.push_front(nome);
+=======
                 atendimento.push_front(nome); // Passa na frente da fila
+>>>>>>> 7a4bfeff7adaf7dfab2644a1560ca9acfa7a0944
                 cout << nome << " adicionado(a) na frente da fila." << endl;
                 break;
 
             case 3:
                 if (!atendimento.empty()) {
                     cout << "Atendendo: " << atendimento.front() << endl;
+<<<<<<< HEAD
+                    atendimento.pop_front();
+=======
                     atendimento.pop_front(); // Remove o primeiro da fila
+>>>>>>> 7a4bfeff7adaf7dfab2644a1560ca9acfa7a0944
                 } else {
                     cout << "Nenhum cliente na fila de espera." << endl;
                 }
